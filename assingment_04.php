deda9aeb30cf3ef0219d521efbb56c1ad480369f
elias1435 <elias1435@gmail.com>
2023-10-04 23:00:54 +0600

    initial commit

---
+++ assingment_04.php
@@ -0,0 +1,38 @@
+class Product {
+    // Properties
+    private $id;
+    private $name;
+    private $price;
+
+    public function __construct($id, $name, $price) {
+        // Initialize properties
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    // Method to get formatted price
+    public function getFormattedPrice() {
+        return '$' . number_format($this->price, 2);
+    }
+
+    // Method to show product details
+    public function showDetails() {
+        echo "Product Details:\n";
+        echo "- ID: " . $this->id . "\n";
+        echo "- Name: " . $this->name . "\n";
+        echo "- Price: " . $this->getFormattedPrice() . "\n";
+    }
+}
+
+// Test the Product class
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+
+
+/*
+Product Details:
+- ID: 1
+- Name: T-shirt
+- Price: $19.99
+*/
